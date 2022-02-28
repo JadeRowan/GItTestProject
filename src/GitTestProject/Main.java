2adac4d53945ff1c4bce0dd7609f0e2737c4335b
JadeRowan <rougesoul.m@gmail.com>
2022-02-28 21:17:13 +0200

    Second branch changed

--- src/GitTestProject/Main.java
+++ src/GitTestProject/Main.java
@@ -3,6 +3,6 @@ package GitTestProject;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello Mate!");
     }
 }
